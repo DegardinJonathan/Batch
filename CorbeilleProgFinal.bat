e7890847d5e4421b926f811bd0c1b7b7abdab4d6
DegardinJonathan <52489184+DegardinJonathan@users.noreply.github.com>
2019-07-03 15:42:16 +0200

    Update CorbeilleProgFinal.bat

--- CorbeilleProgFinal.bat
+++ CorbeilleProgFinal.bat
@@ -1,5 +1,4 @@
 @echo off
-AT 12:00/EVERY:l,ma,me,j,v,s,d
 set /a "VAR1=0"
 for /r "%systemdrive%\$Recycle.bin" %%i in (*.*)do echo Taille du fichier %%i = %%~zi octets & set /a "VAR1 = VAR1+%%~zi"
 echo Taille : %VAR1% octets
